456d58a88ea73a65baa514b59b6aca0c9232c5dd
eclip5e <viachaslau.akhrem@itechart-group.com>
2016-02-03 20:29:28 +0300

    Added route for single article page.

--- routes/article.js
+++ routes/article.js
@@ -1,10 +1,14 @@
 var router = require('express').Router();
 var Article = require('../models/article');
 
-/* GET home page. */
+
+
+/* GET Article addition form. */
 router.get('/add', isLoggedIn, function(req, res) {
     res.render('article-add', { title: 'Create Article' });
 });
+
+
 
 router.post('/add', isLoggedIn, function(req, res) {
 
@@ -14,12 +18,26 @@ router.post('/add', isLoggedIn, function(req, res) {
     newArticle.body = req.body.articleBody;
 
     newArticle.save(function(err) {
-        if (err)
-            throw err;
+        if (err) return console.error(err);
 
         res.redirect('/');
     });
 });
+
+
+
+// GET | Article page
+router.get('/:id', function (req, res) {
+
+    Article.findOne({ '_id': req.params.id }, function (err, article) {
+        if (err) return console.error(err);
+
+        res.send(article);
+    });
+    
+});
+
+
 
 module.exports = router;
 
